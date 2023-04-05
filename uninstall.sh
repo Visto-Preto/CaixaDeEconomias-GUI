b5591d9fe382d9180d861f9be121175940a43961
Visto-Preto <evppdepf@protonmail.ch>
2023-04-05 17:00:04 -0300

    update

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/env bash
-clear
-rm -rf /data/data/com.termux/files/usr/share/cde
-rm -rf /data/data/com.termux/files/usr/bin/cde
-echo ''
-echo 'CDE desinstalado'
-echo ''
